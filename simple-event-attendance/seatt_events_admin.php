69fd97160c4cc1ca69b9bde0a76ea0d53bd74232
withDave <dave@3cc.org>
2017-05-28 15:51:41 +0100

    Minor UI tweak in admin panel and display form
    Updated time formats in admin panel, and the unregister form

--- simple-event-attendance/seatt_events_admin.php
+++ simple-event-attendance/seatt_events_admin.php
@@ -1,7 +1,7 @@
 <?php global $wpdb; ?>
-<div class="wrap">  
+<div class="wrap">
 <?php include("seatt_header.php"); ?>
-          <?php    echo "<h2>" . __( 'Simple Event Attendance Summary', 'seatt_trdom' ) . "</h2>"; ?>  
+          <?php    echo "<h2>" . __( 'Simple Event Attendance Summary', 'seatt_trdom' ) . "</h2>"; ?>
 <?php
 
 // Clean down event ID by checking if numeric, then casting to integer
@@ -21,17 +21,17 @@ if (($event_id != '') && (isset($_GET['remove_event']))) {
 	if ($wpdb->delete($wpdb->prefix.'seatt_events', array('id' => $event_id), array('%d'))) {
 		// If we were able to remove the event, remove any attendees
 		$wpdb->delete($wpdb->prefix.'seatt_attendees', array('event_id' => $event_id), array('%d'));
-		?>  
+		?>
 		<div class="updated">
 			<p><strong>Event <em><?php echo esc_html($event_id); ?></em> deleted.</strong></p>
-		</div>  
+		</div>
 		<?php
 	} else {
 		// If event couldn't be removed
-		?>  
+		?>
 		<div class="error">
 			<p><strong>Event not found. Click <a href="admin.php?page=seatt_events">here</a> to reload this page.</strong></p>
-		</div>  
+		</div>
 		<?php
 	}
 }
@@ -46,10 +46,10 @@ if (($event_id != '') && (isset($_GET['duplicate_event']))) {
 	if ($event != NULL) {
 	// Create another record
 	$wpdb->insert($wpdb->prefix.'seatt_events', array( 'event_name' => $event->event_name, 'event_desc' => $event->event_desc, 'event_limit' => $event->event_limit, 'event_start' => $event->event_start, 'event_expire' => $event->event_expire, 'event_status' => 0, 'event_reserves' => $event->event_reserves ), array('%s', '%s', '%d', '%s', '%s', '%d', '%d') );
-	?>  
+	?>
     <div class="updated">
     	<p><strong>Event <em><?php echo esc_html($event_id); ?></em> duplicated.</strong></p>
-    </div>  
+    </div>
         <?php
 	}
 }
@@ -60,26 +60,34 @@ if (isset($_POST['seatt_hidden'])) {
 } else {
 	$seatt_hidden = 'N';
 }
-if	($seatt_hidden == 'Y') {  
-	//Form data sent  
-	// $data = $_POST['seatt_data'];  
-	//update_option('seatt_dbhost', $dbhost);  
-	?>  
-	<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
-	<?php  
-	}  
+if	($seatt_hidden == 'Y') {
+	//Form data sent
+	// $data = $_POST['seatt_data'];
+	//update_option('seatt_dbhost', $dbhost);
+	?>
+	<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+	<?php
+	}
 
-// Page content start	
-?>	
-<p>To include an attendance form on a page/post, paste the tag into the document text:</p>
-<p> [seatt-form event_id=x] </p>
+// Page content start
+?>
+<p>This page lists all events (open and closed) that exist in the database.</p>
+<br />
+<h2>How to display events in posts and pages</h2>
+<!--<p>To display all currently active events, paste the following into the post/page text:</p>
+<p><pre>[seatt-list]</pre></p>
+<br />-->
+<p>To include a specific attendance form on a page/post, paste the tag into the post/page text:</p>
+<p><pre>[seatt-form event_id=x]</pre></p>
 <p>(x = the event id in the table below). If you haven't created the event_id, or have deleted it, nothing will be displayed.</p>
+<br />
+<h2>Events</h2>
 <table width="auto" border="0" align="left" cellpadding="5" cellspacing="5">
     <tr>
         <th align="left" scope="col">Event ID</th>
         <th align="left" scope="col">Shortcode</th>
         <th align="left" scope="col">Event Name</th>
-        <th align="left" scope="col">Hours Left</th>
+        <th align="left" scope="col">Time Left</th>
         <th align="left" scope="col">Close Date/Time</th>
         <th align="left" scope="col">Status</th>
         <th align="left" scope="col">Attendees</th>
@@ -90,17 +98,24 @@ if	($seatt_hidden == 'Y') {
     $events = $wpdb->get_results("SELECT id, event_name, event_limit, event_expire, event_status FROM ".$wpdb->prefix."seatt_events ORDER BY id DESC");
     foreach ($events as $event) {
 		$attendees = $wpdb->get_var($wpdb->prepare("SELECT COUNT(id) FROM ".$wpdb->prefix."seatt_attendees WHERE event_id = %d", $event->id));
-		$event_expire = ceil($event->event_expire - current_time('timestamp')) / 3600;
-		if ($event_expire < 0) {
-			$event_expire = 0;
+
+    // Create and format the time remaining (ceil to take to zero in case this is negative)
+    $event_expire_seconds = ceil($event->event_expire - current_time('timestamp'));
+
+    // If zero then mark event as expired
+		if ($event_expire_seconds < 0) {
+			$event_expire_seconds = 0;
 			$event->event_status = 0;
     	}
+
+    // Set variable with formatted time remaining
+    $event_expire = sprintf('%02d%s%02d%s%02d%s', floor($event_expire_seconds/3600), 'd ', ($event_expire_seconds/60)%60, 'm ', $event_expire_seconds%60, 's')
     ?>
     <tr>
         <td><?php echo esc_html($event->id); ?></td>
         <td>[seatt-form event_id=<?php echo esc_html($event->id); ?>]</td>
         <td><a href="admin.php?page=seatt_events_edit&event_id=<?php echo intval($event->id); ?>"><?php echo esc_html($event->event_name); ?></a></td>
-        <td><?php echo round($event_expire); ?></td>
+        <td><?php echo $event_expire; ?></td>
         <td><?php echo date("d-m-Y H:i", $event->event_expire); ?></td>
         <td><img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/plugins/simple-event-attendance/<?php echo intval($event->event_status); ?>.gif" width="10" height="10" /></td>
         <td><strong><?php echo intval($attendees); ?></strong> / <?php echo intval($event->event_limit); ?></td>
@@ -108,10 +123,10 @@ if	($seatt_hidden == 'Y') {
     </tr>
     <?php
     }
-	
+
 	if (count($events) == 0) {
 	echo "<tr><td colspan=\"8\">No events found.</td></tr>";
 }
     ?>
 </table>
-</div>  
+</div>
